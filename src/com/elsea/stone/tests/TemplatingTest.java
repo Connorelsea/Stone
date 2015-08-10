1ca58b09724b71cd24c52f17fb5ce8139610b11c
Connorelsea <connorelsea@gmail.com>
2015-08-09 18:09:27 -0700

    Adding many helpful search methods

--- src/com/elsea/stone/tests/TemplatingTest.java
+++ src/com/elsea/stone/tests/TemplatingTest.java
@@ -2,10 +2,13 @@ package com.elsea.stone.tests;
 
 import static org.junit.Assert.*;
 
-import org.junit.Before;
-import org.junit.Test;
-import org.w3c.dom.Document;
+import java.util.List;
 
+import org.junit.Test;
+
+import com.elsea.stone.property.Property;
+import com.elsea.stone.property.PropertyElement;
+import com.elsea.stone.property.PropertyGroup;
 import com.elsea.stone.property.PropertyPool;
 
 public class TemplatingTest {
@@ -47,6 +50,34 @@ public class TemplatingTest {
 		assertEquals("Parent element exists", "parent", p.getParent().getName());
 		
 		p.showXML();
+		
+		// Search for child elements of certain group
+		
+		List<PropertyElement> list = p.search().getElementsInGroup("behavior");
+		
+		for (PropertyElement e : list) System.out.println(e.getName());
+		
+		// Search for specific property
+		
+		Property prop = p.search().getProperty("Close");
+		
+		System.out.println(prop.getCurrentValue());
+		
+		// Search for specific group
+		
+		PropertyGroup group = p.search().getGroup("info");
+		
+		System.out.println(group.getChildAt(1).getName());
+		
+		// Search for duplicates
+		
+		boolean hasDuplicates  = p.search().hasDuplicatesOf("Name");
+		
+		System.out.println(hasDuplicates);
+		
+		boolean hasDuplicates2 = p.search().hasDuplicatesOf("behavior");
+		
+		System.out.println(hasDuplicates2);
 		
 	}
 	
