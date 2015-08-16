32b6ec9e5492a5eff8cbfbfed51a61438bbfbf33
Connorelsea <connorelsea@gmail.com>
2015-08-15 23:26:55 -0500

    Started Stone 2.0 implementation test.

---
+++ src/com/elsea/stone/tests/ImplementationTest.java
@@ -0,0 +1,15 @@
+package com.elsea.stone.tests;
+
+import org.junit.Test;
+
+import com.elsea.stone.groups.Group;
+
+public class ImplementationTest {
+	
+	@Test
+	public void implement()
+	{
+		Group g = new Group();
+	}
+	
+}
